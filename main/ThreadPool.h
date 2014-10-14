219442f5f8a92d3034e0d29fd7e6692e92828a8e
Neraverin <neraverin@gmail.com>
2014-10-14 10:00:11 +0400

    Оформил кода в соответствии с общепринятыми нормами

--- main/ThreadPool.h
+++ main/ThreadPool.h
@@ -1,99 +1,112 @@
-template<typename func_type> class ThreadsPool
+template<typename FuncType> class ThreadPool
 {
-	std::multimap <int, std::function<func_type()> > functionMap;
-	int threadCount = 0;
-	int poolSize = 2;
-	void Handler();
-	void Start(int size );
-
 public:
-	std::mutex taskLock;
-	int getPoolSize();
-	void setPoolSize(int);
-	void push_back(std::function<func_type()> , int priority = 0);
-	int GetThreadCount();
-	const ThreadsPool<func_type>& operator+=(std::function<func_type()> f);
-	const ThreadsPool<func_type>& operator+(std::function<func_type()> f);
-	ThreadsPool();
+    ThreadPool();
+
+    int GetPoolSize();
+    void SetPoolSize(int);
+    int GetThreadCount();
+
+    void PushBack(std::function<FuncType()>, int priority = 0);
+    const ThreadPool<FuncType>& operator += (std::function<FuncType()> f);
+    const ThreadPool<FuncType>& operator + (std::function<FuncType()> f);
+
+private:
+    void Handler();
+    void Start(int size);
+
+    std::multimap <int, std::function<FuncType()> > functionMap;
+    std::mutex taskLock;
+    int RunningThreadCount = 0;
+    int poolSize = 2;
 };
 
-
-
-template<typename func_type>
-void ThreadsPool<func_type>::Handler()
+#pragma region Constructor
+template<typename FuncType>
+ThreadPool<FuncType>::ThreadPool()
 {
-	while (true)
-	{
-		taskLock.lock();
-		auto functionIt = functionMap.begin();
-		if (functionIt == functionMap.end())
-		{
-			taskLock.unlock();
-			break;
-		}
-		auto functionPtr = functionIt->second;
-		functionMap.erase(functionIt);
-		threadCount++;
-		taskLock.unlock();
-		functionPtr();
-		threadCount--;
-	}
+    Start(poolSize);
+}
+#pragma endregion Constructor
+
+#pragma region Get And Set Methods
+template<typename FuncType>
+int ThreadPool<FuncType>::GetPoolSize()
+{
+    return poolSize;
 }
 
-template<typename func_type>
-void ThreadsPool<func_type>::Start(int size)
+template<typename FuncType>
+void ThreadPool<FuncType>::SetPoolSize(int size)
 {
-	for (int i = 0; i < size; i++)
-	boost::thread t1(&ThreadsPool<func_type>::Handler, this);
+    taskLock.lock();
+    Start(size - poolSize);
+    poolSize = size;
+    taskLock.unlock();
 }
 
-template<typename func_type>
-int ThreadsPool<func_type>::getPoolSize()
+template<typename FuncType>
+int ThreadPool<FuncType>::GetThreadCount()
 {
-	return poolSize;
+    return RunningThreadCount;
+}
+#pragma endregion Get And Set Methods
+
+#pragma region Public Methods
+template<typename FuncType>
+void ThreadPool<FuncType>::PushBack(std::function<FuncType()> f, int priority = 0)
+{
+    taskLock.lock();
+    functionMap.insert(std::make_pair(-priority, f));
+    taskLock.unlock();
+}
+#pragma endregion Public Methods
+
+#pragma region Operators
+template<typename FuncType>
+const ThreadPool<FuncType>& ThreadPool<FuncType>::operator+=(std::function<FuncType()> f)
+{
+    PushBack(f);
+    return *this;
 }
 
-template<typename func_type>
-void ThreadsPool<func_type>::setPoolSize(int size)
+template<typename FuncType>
+const ThreadPool<FuncType>& ThreadPool<FuncType>::operator+(std::function<FuncType()> f)
 {
-	taskLock.lock();
-	Start(size-poolSize)
-	poolSize = size;
-	taskLock.unlock();
+    PushBack(f);
+    return *this;
+}
+#pragma endregion Operators
+
+#pragma region Private Methods
+template<typename FuncType>
+void ThreadPool<FuncType>::Handler()
+{
+    while (true)
+    {
+        taskLock.lock();
+        auto functionIt = functionMap.begin();
+        if (functionIt == functionMap.end())
+        {
+            taskLock.unlock();
+            break;
+        }
+        auto functionPtr = functionIt->second;
+        functionMap.erase(functionIt);
+        RunningThreadCount++;
+        taskLock.unlock();
+        functionPtr();
+        RunningThreadCount--;
+    }
 }
 
-template<typename func_type>
-void ThreadsPool<func_type>::push_back(std::function<func_type()> f, int priority = 0)
+template<typename FuncType>
+void ThreadPool<FuncType>::Start(int size)
 {
-	taskLock.lock();
-	functionMap.insert(std::make_pair(-priority, f));
-	taskLock.unlock();
+    for (int i = 0; i < size; i++)
+    {
+        std::thread t1(&ThreadPool<FuncType>::Handler, this);
+        // WARNING
+    }
 }
-
-template<typename func_type>
-int ThreadsPool<func_type>::GetThreadCount()
-{
-	return threadCount;
-}
-
-template<typename func_type>
-const ThreadsPool<func_type>& ThreadsPool<func_type>::operator+=(std::function<func_type()> f)
-{
-	push_back(f);
-	return *this;
-}
-
-template<typename func_type>
-const ThreadsPool<func_type>& ThreadsPool<func_type>::operator+(std::function<func_type()> f)
-{
-	push_back(f);
-	return *this;
-}
-
-template<typename func_type>
-ThreadsPool<func_type>::ThreadsPool()
-{
-	Start(poolSize);
-}
-
-
+#pragma endregion Private Methods
--- main/main.cpp
+++ main/main.cpp
@@ -1,25 +1,23 @@
 #include "stdafx.h"
+
 #include "Task.h"
 #include "ThreadPool.h"
 
-
-
 int main()
 {
-	Task Tasks;
-	typedef void func_type;
-	std::function<func_type()>ptr;
-	ThreadsPool<func_type> Pool1;
-	ptr = std::bind(&Task::FirstTask,Tasks,2, 0.5);
-	Pool1 +=ptr;
-	ptr = std::bind(&Task::SecondTask, Tasks);
-	Pool1+=ptr;
-	ptr = std::bind(&Task::ThirdTask, Tasks, 0, "Strangers");
-	Pool1+=ptr;
-	Pool1 + ptr;
-	ptr = std::bind(&Task::FiveTask, Tasks, 0, "Strangers");
-	Pool1.push_back(ptr, 10);
-	Sleep(10000);
-	return 0;
+    typedef void func_type;
+    std::function<func_type()>ptr;
+    ThreadPool<func_type> exPool;
+    ptr = std::bind(&FirstTask, 10, 1.0);
+    exPool += ptr;
+    ptr = &SecondTask;
+    exPool += ptr;
+    ptr = std::bind(&ThirdTask, 30, "3");
+    exPool += ptr;
+    ptr = std::bind(&ThirdTask, 20, "4");
+    exPool + ptr;
+    ptr = std::bind(&FouthTask, 50, "Five");
+    exPool.PushBack(ptr, 10);
+    Sleep(10000);
+    return 0;
 }
-
